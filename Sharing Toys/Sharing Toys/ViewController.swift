283d8d1651b1b25f82447a71cee941a19337b48d
Geraldo Sales <falecom@geraldosales.com.br>
2021-09-05 15:48:19 -0300

    Creating project with SplashScreen

---
+++ Sharing Toys/Sharing Toys/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sharing Toys
+//
+//  Created by Geraldo O Sales Jr on 05/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
